b80ad7b70ea487dffa5a3458e381b8fb417cd92d
Quentin Honerchick <honerchick.quentin@icloud.com>
2018-09-30 20:36:13 +0200

    Ajout class Test

---
+++ Test du source control/test.swift
@@ -0,0 +1,16 @@
+//
+//  test.swift
+//  Test du source control
+//
+//  Created by Quentin Honerchick on 30/09/2018.
+//  Copyright © 2018 Quentin Honerchick. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    
+    func Salut()  {
+        print ("Salut")
+    }
+}
